77703c58d3584b7f6e54478376ef0f7195e4f724
David Weiland <weiland.david@icloud.com>
2022-02-17 11:56:01 +0100

    reduce colors

--- src/Styles/colors.jsx
+++ src/Styles/colors.jsx
@@ -1,13 +1,8 @@
 const colors = {
-  primary: '#33B5CC',
-  primary20: '#D4F0F5',
-  secondary: '#61C78C',
-  secondary20: '#DFF4E9',
-  tertiary: '#A8E4DF',
-  light: '#F9F9FC',
-  backgroundDark: '#474747',
-  dark: '#2F2E41',
-
+    primary: '#33B5CC',
+    secondary: '#D4F0F5',
+    light: '#F9F9FC',
+    dark: '#2F2E41',
 }
 
 export default colors
